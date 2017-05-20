0b98851c5782eaf0e9c62103c8df5d8b5a349457
kelwyj780304 <kelwyj@kelwyj.(none)>
2017-05-20 16:15:20 -0400

    Introduction-to-Data-Science-in-python

--- leetcode/Minimum-Moves-to-Equal-Array-Elements.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def minMoves(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        n=len(nums)
-        return sum(nums)-n*min(nums)
